76bbe94ca1e10c0794e920c9e5a209e1945f060a
James Bristow <JBris@users.noreply.github.com>
2023-03-17 03:37:09 +1300

    Update install_apptainer.sh

--- install_apptainer.sh
+++ install_apptainer.sh
@@ -19,3 +19,7 @@ cd /tmp && \
     wget https://github.com/apptainer/apptainer/releases/download/v${APPTAINER_VERSION}/apptainer-suid_${APPTAINER_VERSION}_amd64.deb && \
     sudo dpkg -i ./apptainer-suid_${APPTAINER_VERSION}_amd64.deb
     
+cd /tmp && \
+    apptainer pull docker://ghcr.io/apptainer/lolcow && \ 
+    apptainer exec lolcow_latest.sif cowsay moo
+    
