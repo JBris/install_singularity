63d4adf3c81009b591da16c801f7d05e5296ed0b
James Bristow <JBris@users.noreply.github.com>
2023-03-16 17:37:20 +1300

    Add files via upload

---
+++ install_apptainer.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+###################################################################
+# Constants
+###################################################################
+
+APPTAINER_VERSION=1.1.6
+
+###################################################################
+# Main
+###################################################################
+
+sudo apt update
+sudo apt install -y wget
+
+cd /tmp && \
+    wget https://github.com/apptainer/apptainer/releases/download/v${APPTAINER_VERSION}/apptainer_${APPTAINER_VERSION}_amd64.deb && \
+    sudo apt install -y ./apptainer_${APPTAINER_VERSION}_amd64.deb && \
+    wget https://github.com/apptainer/apptainer/releases/download/v${APPTAINER_VERSION}/apptainer-suid_${APPTAINER_VERSION}_amd64.deb && \
+    sudo dpkg -i ./apptainer-suid_${APPTAINER_VERSION}_amd64.deb
+    
